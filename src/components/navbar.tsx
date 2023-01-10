a49be8afb4fbf6d37bade16a005f5556b6e78031
Denny Setyawan Huda <dennshgit@gmail.com>
2023-01-10 18:03:42 +0700

    feat: create navbar section

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,46 @@
 export function Navbar() {
   return (
-    <>
-      <nav>
-        <h1 className="font-bold">Bold</h1>
-        <h1 className="font-semibold">Semi</h1>
-      </nav>
-    </>
+    <div className="px-4 py-3 fixed top-0 left-0 w-full">
+      <div className="container mx-auto">
+        <nav className="flex justify-between items-center">
+          <div className="border-l-4 border-[#203271]">
+            <a className="font-semibold ml-2 text-2xl text-[#101C45]" href="/">
+              PRIMATEKNIK
+            </a>
+          </div>
+          <div className="text-[#101C45] opacity-80">
+            <a
+              className="mx-3 hover:border-b-2 hover:border-[#101C45]"
+              href="/"
+            >
+              Home
+            </a>
+            <a
+              className="mx-3 hover:border-b-2 hover:border-[#101C45]"
+              href="#about"
+            >
+              About
+            </a>
+            <a
+              className="mx-3 hover:border-b-2 hover:border-[#101C45]"
+              href="#services"
+            >
+              Services
+            </a>
+            <a
+              className="mx-3 hover:border-b-2 hover:border-[#101C45]"
+              href="#gallery"
+            >
+              Gallery
+            </a>
+          </div>
+          <div className="bg-[#203271] px-3 py-2 rounded-md">
+            <a className="text-white" href="#contact">
+              Contact
+            </a>
+          </div>
+        </nav>
+      </div>
+    </div>
   );
 }
