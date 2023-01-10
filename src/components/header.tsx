294171c9439ee246ca01d8b8d817eb6f490cfc0d
Denny Setyawan Huda <dennshgit@gmail.com>
2023-01-10 22:14:23 +0700

    feat: add header responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,12 +7,12 @@ import stampford from "../assets/brands/stampford.svg";
 export function Header() {
   return (
     <div className="container mx-auto px-5">
-      <div className="h-[70vh] flex items-end justify-between">
-        <div className="w-1/2">
-          <h1 className="text-7xl font-bold w-[40rem] ">
+      <div className="xl:h-[70vh] flex xl:items-end xl:justify-between flex-col xl:flex-row">
+        <div className="xl:w-1/2 mt-[5rem] text-center xl:text-left order-2 xl:order-none">
+          <h1 className="text-3xl md:text-5xl xl:text-7xl font-bold xl:w-[40rem] ">
             Prima Teknik Lombok Siap Membantu Anda
           </h1>
-          <p className="w-[30rem] my-7 text-2xl">
+          <p className="xl:w-[30rem] my-7 xl:text-2xl">
             Melayani service, jual beli, penyewaan, perawatan & instalasi genset
             di wilayah Lombok, Sumbawa, dan Bima.
           </p>
@@ -23,23 +23,34 @@ export function Header() {
             Hubungi Kami
           </a>
         </div>
-        <div>
-          <img className="w-[35rem]" src={picture} alt="header picture" />
+        <div className="mt-[10rem] mx-auto">
+          <img className="xl:w-[35rem]" src={picture} alt="header picture" />
         </div>
       </div>
-      <div className="h-[30vh] flex justify-between items-center">
-        <div>
-          <img className="h-[10rem]" src={cummins} alt="Cummins Logo" />
-        </div>
-        <div>
-          <img className="h-[10rem]" src={perkins} alt="Cummins Logo" />
-        </div>
-        <div>
-          <img className="h-[10rem]" src={stampford} alt="Cummins Logo" />
-        </div>
-        <div>
-          <img className="h-[5rem]" src={caterpillar} alt="Cummins Logo" />
-        </div>
+      <div className="mt-[5rem] xl:mt-0 xl:h-[30vh] flex xl:justify-between xl:items-center justify-center flex-col xl:flex-row">
+        <img
+          className="h-[5rem] md:h-[10rem]"
+          src={cummins}
+          alt="Cummins Logo"
+        />
+
+        <img
+          className="h-[5rem] md:h-[10rem]"
+          src={perkins}
+          alt="Cummins Logo"
+        />
+
+        <img
+          className="h-[5rem] md:h-[10rem]"
+          src={stampford}
+          alt="Cummins Logo"
+        />
+
+        <img
+          className="h-[2rem] md:h-[5rem]"
+          src={caterpillar}
+          alt="Cummins Logo"
+        />
       </div>
     </div>
   );
