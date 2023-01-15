2c02d2959004fca7c7ca89041dda3836c9104b1d
Denny Setyawan Huda <dennshgit@gmail.com>
2023-01-16 06:27:36 +0700

    feat: add footer section

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,7 @@ import { Header } from "./components/header";
 import { About } from "./components/about";
 import { Services } from "./components/services";
 import { Contact } from "./components/contact";
+import { Footer } from "./components/footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Services />
       <Contact />
+      <Footer />
     </div>
   );
 }
