74d170dc10e8da7231e4f7074f0cf7c35f17d527
Denny Setyawan Huda <dennshgit@gmail.com>
2023-01-16 10:03:31 +0700

    feat: data services

---
+++ src/data/index.ts
@@ -0,0 +1,34 @@
+const genset = [
+  {
+    title: "Rental Genset 10-300 kVa",
+    description: "Menyewakan berbagai kebutuhan unit genset",
+    imageUrl:
+      "https://drive.google.com/uc?export=view&id=1sutQtrr1mW4SD66KX_wPyTckL2JZesrj",
+  },
+  {
+    title: "Jual Beli Genset",
+    description: "Jual Beli Genset berbagai tipe dan bergaransi",
+    imageUrl:
+      "https://drive.google.com/uc?export=view&id=1EwJ_qiCMI1L7JtKHE0VUngFOGCm7Z-Qe",
+  },
+  {
+    title: "Rental Alat Berat",
+    description: "Menyewakan alat berat seperti crane, forklift dll",
+    imageUrl:
+      "https://drive.google.com/uc?export=view&id=1IevzvMNJffyaU_27wS_fY3Li1M2Ln3pi",
+  },
+  {
+    title: "Jasa Service",
+    description: "Menyediakan jasa service mesin",
+    imageUrl:
+      "https://drive.google.com/uc?export=view&id=1Z5Z5S837gRCYgp3V5Pkb9tkwUtCn3cSM",
+  },
+  {
+    title: "Menjual Spare Spart",
+    description: "Menjual berbagai jenis spare part",
+    imageUrl:
+      "https://drive.google.com/uc?export=view&id=1Cnbtk0PjjtAxNdQMEAsJPdQx1QVW75JD",
+  },
+];
+
+export default genset;
